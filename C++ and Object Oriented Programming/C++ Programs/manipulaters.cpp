c5b5d5c874dab83541e9406b3ed5fe4da573bb86
Jap <jeet63240@gmail.com>
2021-12-23 12:09:08 +0530

    Make endl manipulator workable in vscode.

--- C++ and Object Oriented Programming/C++ Programs/manipulaters.cpp
+++ C++ and Object Oriented Programming/C++ Programs/manipulaters.cpp
@@ -1,13 +1,12 @@
-#include<iostream>
-#include<iomanip>
+#include <iostream>
+#include <iomanip>
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-	int a =123;
+	int a = 123;
 	int b = 12345;
 	int c = 1234;
-	std::cout<<"A = "<<setw(5)<<a<<endl;
-	std::cout<<"B = "<<setw(5)<<b<<endl;
-	std::cout<<"C = "<<setw(5)<<c<<endl;
+	std::cout << "A = " << std::setw(5) << a << std::endl;
+	std::cout << "B = " << std::setw(5) << b << std::endl;
+	std::cout << "C = " << std::setw(5) << c << std::endl;
 }
-
